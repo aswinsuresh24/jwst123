08842ae2b91a6e68be0c322f12c9b59859dad190
aswin <aswinsuresh4869@gmail.com>
2024-09-22 16:08:45 -0500

    add nircammask and calsky

--- jwst123.py
+++ jwst123.py
@@ -26,6 +26,7 @@ from scipy.interpolate import interp1d
 from jwst.pipeline import calwebb_image3
 import jhat
 from jhat import jwst_photclass,st_wcs_align
+import subprocess
 
 # Internal dependencies
 from common import Constants
@@ -67,16 +68,43 @@ def get_detector_chip(filename):
     
     return None
 
+def apply_nircammask(files):
+    cmd = ['nircammask', '-etctime']
+    for fl in files:
+        # cmd += f' {fl}'
+        cmd.append(fl)
+
+    subprocess.run(cmd)  
+
+def calc_cky(files):
+    cmd = 'calcsky {fits_base} {rin} {rout} {step} {sigma_low} {sigma_high}'
+    for fl in files:
+        #read params from options later
+        fits_base = fl.replace('.fits','')
+        print(fits_base)
+        rin = 15
+        rout = 25
+        step = -64
+        sigma_low = 2.25
+        sigma_high = 2.00
+        cmd_fl = cmd.format(fits_base=fits_base,rin=rin,rout=rout,
+                            step=step,sigma_low=sigma_low,sigma_high=sigma_high)
+        subprocess.run(cmd_fl,shell=True)
+
+
 if __name__ == '__main__':
     root_dir = '.'
+    align_method = 'relative' #'relative' or 'gaia'
     
-    input_images = glob.glob('jwstred_temp_mosaic/*cal.fits')
+    input_images = glob.glob('jwstred_temp_mosaic/*cal.fits') #gen
     table = input_list(input_images)
     tables = organize_reduction_tables(table, byvisit=False)
 
     align_images = np.array([r['image'] for r in table])
 
-    ref_mask = ['0001' in r for r in align_images]
+    # Align image
+
+    ref_mask = ['0001' in r for r in align_images] #change to properly pick refs
     ref_images = align_images[ref_mask]
     print(ref_images)
 
@@ -92,53 +120,58 @@ if __name__ == '__main__':
 
     for align_image in align_images:
 
-        if align_image in ref_images:
-            continue
+        if align_method == 'relative':
+            print('Running relative alignment')
+            if align_image in ref_images:
+                continue
 
-        chip = get_detector_chip(align_image)
-        ref_image = ref_dict[chip]
-        ref_catname = ref_image.replace('.fits','.phot.txt')
-        refcat = Table.read(ref_catname,format='ascii')
+            chip = get_detector_chip(align_image)
+            ref_image = ref_dict[chip]
+            ref_catname = ref_image.replace('.fits','.phot.txt')
+            refcat = Table.read(ref_catname,format='ascii')
 
-        wcs_align.run_all(align_image,
-                telescope='jwst',
-                outsubdir='jwstred_temp_mosaic',    
-                refcat_racol='ra',
-                refcat_deccol='dec',
-                refcat_magcol='mag',
-                refcat_magerrcol='dmag',
-                overwrite=True,
-                d2d_max=.25,
-                showplots=0,
-                find_stars_threshold=3,
-                refcatname=ref_catname,
-                histocut_order='dxdy',
-                    sharpness_lim=(0.3,0.9),
-                    roundness1_lim=(-0.7, 0.7),
-                    SNR_min= 5,
-                    dmag_max=0.1,
-                    objmag_lim =(14,21))
+            wcs_align.run_all(align_image,
+                    telescope='jwst',
+                    outsubdir='jwstred_temp_mosaic',    #gen
+                    refcat_racol='ra',
+                    refcat_deccol='dec',
+                    refcat_magcol='mag',
+                    refcat_magerrcol='dmag',
+                    overwrite=True,
+                    d2d_max=.25,
+                    showplots=0,
+                    find_stars_threshold=3,
+                    refcatname=ref_catname,
+                    histocut_order='dxdy',
+                        sharpness_lim=(0.3,0.9),
+                        roundness1_lim=(-0.7, 0.7),
+                        SNR_min= 5,
+                        dmag_max=0.1,
+                        objmag_lim =(14,21))
 
-    #gaia
-    #     wcs_align.run_all(align_image,
-    #                     telescope='jwst',
-    #                     outsubdir='jwstred_temp+mosaic',
-    #                     overwrite=True,
-    #                     d2d_max=1,
-    #                     find_stars_threshold = 3,
-    #                     showplots=0,
-    #                     refcatname='Gaia',
-    #                     histocut_order='dxdy',
-    #                         sharpness_lim=(0.3,0.9),
-    #                         roundness1_lim=(-0.7, 0.7),
-    #                         SNR_min= 3,
-    #                         dmag_max=1,
-    #                         Nbright = 150,
-    #                         objmag_lim =(14,20))
+        if align_method == 'gaia':
+            print('Running Gaia alignment')
+            wcs_align.run_all(align_image,
+                            telescope='jwst',
+                            outsubdir='jwstred_temp_mosaic',  #gen
+                            overwrite=True,
+                            d2d_max=1,
+                            find_stars_threshold = 3,
+                            showplots=0,
+                            refcatname='Gaia',
+                            histocut_order='dxdy',
+                                sharpness_lim=(0.3,0.9),
+                                roundness1_lim=(-0.7, 0.7),
+                                SNR_min= 3,
+                                dmag_max=1,
+                                Nbright = 150,
+                                objmag_lim =(14,20))
+
+    # Mosaic images into reference image
 
     filter_name = table[0]['filter']
-    nircam_asn_file = f'jwstred_temp_mosaic/{filter_name}.json'
-    base_filenames = np.array([os.path.basename(r) for r in glob.glob('jwstred_temp_mosaic/*jhat.fits')])
+    nircam_asn_file = f'jwstred_temp_mosaic/{filter_name}.json'  #gen
+    base_filenames = np.array([os.path.basename(r) for r in glob.glob('jwstred_temp_mosaic/*jhat.fits')]) #gen
     asn3 = asn_from_list.asn_from_list(base_filenames, 
         rule=DMS_Level3_Base, product_name=filter_name)
     
@@ -148,7 +181,7 @@ if __name__ == '__main__':
 
     image3 = calwebb_image3.Image3Pipeline()
 
-    outdir_level3 = os.path.join('jwstred_temp_mosaic/', 'out')
+    outdir_level3 = os.path.join('jwstred_temp_mosaic/', 'out')    #gen
     if not os.path.exists(outdir_level3):
         os.makedirs(outdir_level3)
 
@@ -160,3 +193,28 @@ if __name__ == '__main__':
     image3.source_catalog.skip=False
 
     image3.run(nircam_asn_file)
+
+    # move to dolphot directory (generalize paths later)
+
+    outdir = 'jwstred_temp_dolphot/m92/nircam'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    
+    outdir_raw = os.path.join(outdir, 'raw')
+    if not os.path.exists(outdir_raw):
+        os.makedirs(outdir_raw)
+
+    for fl in glob.glob('jwstred_temp_mosaic/*cal.fits'):
+        shutil.copy(fl, outdir)
+        shutil.copy(fl, outdir_raw)
+
+    for fl in glob.glob('jwstred_temp_mosaic/out/*i2d.fits'):
+        shutil.copy(fl, outdir)
+        shutil.copy(fl, outdir_raw)
+
+    # run nircammask (generalize paths later)
+
+    basedir = 'jwstred_temp_dolphot/m92/nircam'
+    dolphot_images = glob.glob(basedir + '/*fits')
+    apply_nircammask(dolphot_images)
+    calc_cky(dolphot_images)
