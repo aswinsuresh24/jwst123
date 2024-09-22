2cc578e6a3c67e7abc1c8b6d78fc0cee7cd6fc64
aswin <aswinsuresh4869@gmail.com>
2024-09-22 11:28:45 -0500

    create m92 mosaic with relative alignment

--- jwst123.py
+++ jwst123.py
@@ -58,19 +58,68 @@ from jwst.associations import asn_from_list
 from jwst.associations.lib.rules_level3_base import DMS_Level3_Base
 print(f'JWST version: {jwst.__version__}')
 
+def get_detector_chip(filename):
+    fl_split = filename.split('_')
+    mask = ['nrc' in x for x in fl_split]
+    if any(mask):
+        idx = mask.index(True)
+        return fl_split[idx]
+    
+    return None
+
 if __name__ == '__main__':
     root_dir = '.'
-    filter_name = 'F115W'
-
-    input_images = glob.glob('jwstred_temp+mosaic/*cal.fits')
+    
+    input_images = glob.glob('jwstred_temp_mosaic/*cal.fits')
     table = input_list(input_images)
     tables = organize_reduction_tables(table, byvisit=False)
 
-    # align_images = np.array([r['image'] for r in table])
-    # print(align_images)
-    # wcs_align = st_wcs_align()
+    align_images = np.array([r['image'] for r in table])
 
-    # for align_image in align_images:
+    ref_mask = ['0001' in r for r in align_images]
+    ref_images = align_images[ref_mask]
+    print(ref_images)
+
+    dets = [get_detector_chip(r) for r in ref_images]
+    ref_dict = dict(zip(dets, ref_images))
+    print(ref_dict)
+
+    for ref_image in ref_images:
+        jwst_phot = jwst_photclass()
+        jwst_phot.run_phot(imagename=ref_image,photfilename='auto',overwrite=True,ee_radius=80)
+
+    wcs_align = st_wcs_align()
+
+    for align_image in align_images:
+
+        if align_image in ref_images:
+            continue
+
+        chip = get_detector_chip(align_image)
+        ref_image = ref_dict[chip]
+        ref_catname = ref_image.replace('.fits','.phot.txt')
+        refcat = Table.read(ref_catname,format='ascii')
+
+        wcs_align.run_all(align_image,
+                telescope='jwst',
+                outsubdir='jwstred_temp_mosaic',    
+                refcat_racol='ra',
+                refcat_deccol='dec',
+                refcat_magcol='mag',
+                refcat_magerrcol='dmag',
+                overwrite=True,
+                d2d_max=.25,
+                showplots=0,
+                find_stars_threshold=3,
+                refcatname=ref_catname,
+                histocut_order='dxdy',
+                    sharpness_lim=(0.3,0.9),
+                    roundness1_lim=(-0.7, 0.7),
+                    SNR_min= 5,
+                    dmag_max=0.1,
+                    objmag_lim =(14,21))
+
+    #gaia
     #     wcs_align.run_all(align_image,
     #                     telescope='jwst',
     #                     outsubdir='jwstred_temp+mosaic',
@@ -88,9 +137,8 @@ if __name__ == '__main__':
     #                         objmag_lim =(14,20))
 
     filter_name = table[0]['filter']
-    nircam_asn_file = f'jwstred_temp+mosaic/{filter_name}.json'
-    # base_filenames = np.array([os.path.basename(r['image']) for r in table])
-    base_filenames = np.array([os.path.basename(r) for r in glob.glob('jwstred_temp+mosaic/*jhat.fits')])
+    nircam_asn_file = f'jwstred_temp_mosaic/{filter_name}.json'
+    base_filenames = np.array([os.path.basename(r) for r in glob.glob('jwstred_temp_mosaic/*jhat.fits')])
     asn3 = asn_from_list.asn_from_list(base_filenames, 
         rule=DMS_Level3_Base, product_name=filter_name)
     
@@ -100,7 +148,7 @@ if __name__ == '__main__':
 
     image3 = calwebb_image3.Image3Pipeline()
 
-    outdir_level3 = os.path.join('jwstred_temp+mosaic/', 'out')
+    outdir_level3 = os.path.join('jwstred_temp_mosaic/', 'out')
     if not os.path.exists(outdir_level3):
         os.makedirs(outdir_level3)
 
